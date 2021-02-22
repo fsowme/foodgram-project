b5615d3164853e8b7fee660dd2f9960f6dfcfd84
Vitalii <fsow@yandex.ru>
2021-02-22 15:12:22 +0300

    load static into templates

--- foodgram/food/views.py
+++ foodgram/food/views.py
@@ -4,5 +4,7 @@ from django.shortcuts import HttpResponse, render
 # Create your views here.
 def testpage(request):
     # return render(request, "test.html")
+    return render(request, "singlePageNotAuth.html")
+    return render(request, "indexAuth.html")
     return render(request, "indexNotAuth.html")
     return HttpResponse("Ok")
