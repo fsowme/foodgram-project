5ed341632d04e1b6c9bc3fafb8af7eaf50b1b87d
Vitalii <fsow@yandex.ru>
2021-07-13 14:17:30 +0300

    fix install docker script (add sudo)

--- foodgram/install_docker.sh
+++ foodgram/install_docker.sh
@@ -2,21 +2,21 @@
 docker=`dpkg -l | grep docker-ce`
 if [ -z "$docker" ]
 then
-    apt-get -y remove docker docker-engine docker.io containerd runc
-    apt-get update
-    apt-get -y install \
+    sudo apt-get -y remove docker docker-engine docker.io containerd runc
+    sudo apt-get update
+    sudo apt-get -y install \
         apt-transport-https \
         ca-certificates \
         curl \
         gnupg-agent \
         software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-    add-apt-repository \
+    sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
-    apt-get update
-    apt-get -y install docker-ce docker-ce-cli containerd.io
+    sudo apt-get update
+    sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 fi
 
 if [ ! -e /usr/local/bin/docker-compose ]
